2e02187504293011734cbfe4c5157ccea9f3d734
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-21 02:04:25 +0200

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,10 +6,10 @@ throw false
 
 if (command == 'tagall' || command == 'invocar' || command == 'todos' || command == 'invocación' || command == 'invocacion') {
 let pesan = args.join` `
-let oi = `@richetti_123 ${pesan}`
-let teks = `${global.packname} te invoca despiertate😡!!\n\n${oi}\n\n`
+let oi = `@darkat97 ${pesan}`
+let teks = `Hola Lobit@s\n\n${oi}\n\n`
 for (let mem of participants) {
-teks += `😼 @${mem.id.split('@')[0]}\n`}
+teks += `🐺 @${mem.id.split('@')[0]}\n`}
 teks += `${global.packname}`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
